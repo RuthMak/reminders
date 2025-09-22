036cf2f753747fa731ae33f0db2d055733fb8d5b
Ruth <143053043+RuthMak@users.noreply.github.com>
2025-09-21 23:23:13 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,42 @@
+import express from 'express';
+import path from "path";
+import { fileURLToPath } from "url";
+
+const app = express();
+const port = 3000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.use(express.static(__dirname + "/public"));
+
+app.set('view engine', 'ejs')
+
+let meetings = [
+    { topic: 'CIT Monthly Meeting', mandatory : true, dateTime: 'September 19th 2024, 2pm-3pm', location: 'Knoy Hall West Lafayette' },
+    { topic: 'Research In Higher Level Ed', mandatory : true, dateTime: 'September 24th 2024, 1pm-5pm', location: 'Beresford Building, Room 2, West Lafayette' },
+    { topic: 'Curriculum Planning', mandatory : true, dateTime: 'October 19th 2024, 4pm-6pm', location: 'IO240, Indianapolis' }
+  ];
+  
+app.get("/", (req, res) => {
+  res.render("pages/index", { data: meetings, title: "Scheduled Meetings",});
+});
+app.get("/about", (req, res) => {
+  res.render("pages/about", { title: "About Us" });
+});
+app.get("/contact", (req, res) => {
+  res.render("pages/contact", { title: "Contact Us" });
+});
+app.listen(port, () => {
+  console.log(`Listening at   port ${port}`);
+});
+
+
+
+/* let dataToPass = { topic: "CIT Monthly Meeting",
+    dateTime: "September 18th 2025, 10am-11am",
+    location: "Knoy Hall West Lafayette"};
+app.get('/', (req, res) => {
+    res.render('pages/index'), { data : dataToPass}
+}); */
+
+
